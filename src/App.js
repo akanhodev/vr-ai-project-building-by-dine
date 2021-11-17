390ae8a96d540c091b9a25a1a37606de8dfa49cc
akanhodev <akanho.dev@gmail.com>
2021-11-17 12:26:00 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import Article from "./components/articles/Article";
+import Brand from "./components/brand/Brand";
+import Cta from "./components/cta/Cta";
+import Features from "./components/features/Features";
+import Navbar from "./components/navbar/Navbar";
+import Blog from "./containers/blog/Blog";
+import Header from "./containers/header/Header";
+import WhatGPT3 from "./containers/whatGPT3/WhatGPT3";
+import Possibility from "./containers/possibility/Possibility";
+import Footer from "./containers/footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>This is vr app !!!</h1> */}
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+
+      <Brand />
+      {/* <Article /> */}
+      <WhatGPT3 />
+
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
