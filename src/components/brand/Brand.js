ede581c4f4aba19517e7a3e924e79a1ba83b570c
akanhodev <akanho.dev@gmail.com>
2021-11-20 23:10:18 +0000

    brand components done

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -5,17 +5,25 @@ import "./Brand.css";
 // } from './imports'
 
 const Brand = () => {
-  return(
-      <div className="gpt3__brand section__padding">
-          <div>
-            <img src="../../assets/google.png" alt="google" />
-            <img src="../../assets/slack.png" alt="slack" />
-            <img src="../../assets/shopify.png" alt="shopify" />
-            <img src="../../assets/dropbox.png" alt="dropbox" />
-            <img src="../../assets/atlassian.png" alt="atlassian" />
-          </div>
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src="../../assets/google.png" alt="google" />
       </div>
-  )
+      <div>
+        <img src="../../assets/slack.png" alt="slack" />
+      </div>
+      <div>
+        <img src="../../assets/shopify.png" alt="shopify" />
+      </div>
+      <div>
+        <img src="../../assets/dropbox.png" alt="dropbox" />
+      </div>
+      <div>
+        <img src="../../assets/atlassian.png" alt="atlassian" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
