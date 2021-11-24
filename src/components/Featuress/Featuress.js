a136b586efd8bdf23e026d236862970d6b3384d7
akanhodev <akanho.dev@gmail.com>
2021-11-24 18:00:12 +0000

    Features done and another possibility started

--- src/components/Featuress/Featuress.js
+++ src/components/Featuress/Featuress.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Features from "../features/Features";
 import "./Featuress.css";
+import Fade from "react-reveal/Fade";
 
 const Featuress = () => {
   const featuresData = [
@@ -24,21 +25,24 @@ const Featuress = () => {
   return (
     <div className="gpt3__features section__padding" id="features">
       <div className="gpt3__features-heading">
-        <h1 className="gradient__text">
-          The Future is Now and You Just Need To Realize It. Step into Future
-          Today & Make it Happen.
-        </h1>
-        <p>Request Early Access to Get Started</p>
+        <Fade left>
+          <h1 className="gradient__text">
+            The Future is Now and You Just Need To Realize It. Step into Future
+            Today & Make it Happen.
+          </h1>
+          <p>Request Early Access to Get Started</p>
+        </Fade>
       </div>
-      <div className="gpt3__features-content">
+      <div className="gpt3__features-container">
         {featuresData?.map((item, index) => (
-          <Features
-            title={item.title}
-            text={item.text}
-            key={item.title * index}
-          />
+          <Fade right>
+            <Features
+              title={item.title}
+              text={item.text}
+              key={item.title * index}
+            />
+          </Fade>
         ))}
-        
       </div>
     </div>
   );
--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -3,8 +3,16 @@ import './Possibility.css'
 
 const Possibility = () => {
     return (
-        <div>
-            Possibility
+        <div className="possibility section__padding" id="possibility">
+            <div className="gpt3__possibility">
+                <img src="./assets/possibility.png" alt="possible" />
+            </div>
+            <div className="gpt3__possibility-content">
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className="text__gradient">The possibilities are beyond your imagination</h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <p>Request Early Access to Get Started</p>
+            </div>
         </div>
     )
 }
